1c2b9a0b3009b5473d685a185d98b17eef48b0a3
Samuel Lando <samuel.lando@aol.com>
2023-04-10 14:44:47 -0400

    Add gcp integration

---
+++ addsample.py
@@ -0,0 +1,16 @@
+import sys
+import firebase_admin
+from firebase_admin import firestore
+import anyrest
+
+import os
+os.environ['GRPC_DNS_RESOLVER'] = 'native'
+
+import sys 
+project = sys.argv[1]
+
+
+firebase_app = firebase_admin.initialize_app(options={'projectId': project})
+db = firestore.client()
+
+anyrest.insert_data_into_firestore({"message": "Hello, World!"}, db.collection("helloworld").document("samplecollection"))
